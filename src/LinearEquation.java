69bccf7e0781027506efe2cf50f7207ffe16bd81
BT_2S14_26 <arshtrashmails@gmail.com>
2024-10-02 10:52:09 -0400

    Initial commit

---
+++ src/LinearEquation.java
@@ -0,0 +1,22 @@
+public class LinearEquation {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+    private int x3;
+
+    public int LinearEquation(int x1, int y1, int x2, int y2) {
+        x1 = this.x1;
+        y1 = this.y1;
+        x2 = this.x1;
+        y2 = this.y1;
+    }
+
+    public int LinearEquation(int x1, int y1, int x2, int y2, int x3) {
+        x1 = this.x1;
+        y1 = this.y1;
+        x2 = this.x1;
+        y2 = this.y1;
+        x3 = this.x3;
+    }
+}
